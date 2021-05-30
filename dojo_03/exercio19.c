3fe8f33ae6827c83c893d273575b55177861ab65
jofelipe <jofelipe@student.42sp.org.br>
2021-05-30 19:59:25 +0000

    exerc19nois

--- dojo_03/exercio19.c
+++ dojo_03/exercio19.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
 
-int main(void)
+int	main(void)
 {
-    printf("Digite a distancia percorrida em km: ");
-    scanf("%f",&km);
-    printf("Digite a quantidade total de litros: ");
-    scanf("%f",&l);
-    result = km / l;
-    if (result < 8)
-    {
-        printf("Venda o carro!");
-    }
-    else {
-        if (result > 8 && result < 14){
-            printf("Econonico!")
-        }
-        else {
-            if (restul >= 14){
-                printf("Super econonico!")
-            }
-        }
-    }
-    return (0);
+	float	km;
+	float	l;
+	float	result;
+
+	printf("Digite a distancia percorrida em km: ");
+	scanf("%f", &km);
+	printf("Digite a quantidade total de litros: ");
+	scanf("%f", &l);
+	result = km / l;
+	if (result < 8)
+	{
+		printf("Venda o carro!");
+	}
+	else if (result > 8 && < 14)
+	{
+		printf("Economico!\n");
+	}
+	else if (result >= 14)
+	{
+		printf("Super econonico!\n");
+	}
+	return (0);
 }

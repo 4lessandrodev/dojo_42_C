e092426f0d90436c4c682c3d962be12cfce83134
Willian Rosendo de Lima <willianrosendodelima@outlook.com>
2021-05-30 14:55:43 -0300

    lá vem

--- dojo_01/main.c
+++ dojo_01/main.c
@@ -2,21 +2,36 @@
 
 int	main(void)
 {
-	int		value_one;
-	int		value_two;
+	int		value_one = 0;
+	int		value_two = 0;
 	char	symbol;
-	float	result;
+	float	result = 0;
 
 	printf("Informe o primeiro valor: ");
 	scanf("%d", &value_one);
 	printf("Informe o segundo valor: ");
 	scanf("%d", &value_two);
-	printf("Informe a operação: +, -, * ou /");
+	printf("Informe a operação: +, -, * ou / \n");
 	scanf("%c", &symbol);
 	if (symbol == '+')
 	{
 		result = value_two + value_one;
 		printf("Soma de %d + %d é: %f", value_one, value_two, result);
+	}
+	if (symbol == '-')
+	{
+		result = value_two - value_one;
+		printf("Subtração de %d - %d é: %f", value_one, value_two, result);
+	}
+	if (symbol == '*')
+	{
+		result = value_two * value_one;
+		printf("Multiplicação de %d * %d é: %f",value_one, value_two, result);
+	}
+	if (symbol == '/')
+	{
+		result = value_two / value_one;
+		printf("Divisão de %d / %d é: %f", value_one, value_two, result);
 	}
 	return (0);
 }

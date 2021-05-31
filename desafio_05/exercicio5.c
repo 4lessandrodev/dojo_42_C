efdaca93807be9c8531f8098b2b7843d415d9b4d
jofelipe <jofelipe@student.42sp.org.br>
2021-05-31 00:46:22 +0000

    segura essa lenha

--- desafio_05/exercicio5.c
+++ desafio_05/exercicio5.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
-int	main(void){
+int	main(void)
 
-int hour;
-int minutes;
-int seconds;
-int result;
+{
+	int	hour;
+	int	minutes;
+	int	seconds;
+	int	result;
 
-printf("Digite o valor de horas: ");
-scanf("%i", &hour);
-printf("Digite o valor de minutos");
-scanf("%i", &minutes);
-print("Digite o valor de segundos");
-scanf("%i", &seconds);
-
-result = hour * 60 * 60;
-result += minutes * 60;
-result += seconds;
-
-printf("O valor total de segundos é: %i\n", result);
-
+	printf("Digite o valor de horas: ");
+	scanf("%i", &hour);
+	printf("Digite o valor de minutos");
+	scanf("%i", &minutes);
+	printf("Digite o valor de segundos");
+	scanf("%i", &seconds);
+	result = hour * 60 * 60;
+	result += minutes * 60;
+	result += seconds;
+	printf("O valor total de segundos é: %i\n", result);
 }
---
+++ desafio_05/exercicio6.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+//prompt user for a whole number
+//print a pyramid of exclamation marks starting from 1 until the whole number
+
+int	main(void)
+{
+	int h
+	int x
+	int h2
+	&2
+	h = 0
+	char = 33
+	scanf("Size of your pyramid %d", h);
+	while (h2 > 0)
+	{
+		printe ! h - h +1
+		printf("%c", )
+		h++
+	}
+}

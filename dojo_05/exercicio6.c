b7f029111decd7f80cadcf6192bd89722e8b1c69
Alessandrodev <alessandroadm@live.com>
2021-05-30 22:00:48 -0300

    feat: added variables

---
+++ dojo_05/exercicio6.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+//prompt user for a whole number
+//print a pyramid of exclamation marks starting from 1 until the whole number
+
+int	main(void)
+{
+	int quantity;
+	char vector[10];
+	char symbol;
+	int provided_value;
+	int count;
+
+	count = 0;
+	symbol = 33;
+	
+	printf("Informe o tamanho da pirâmide");
+	scanf("%d", &quantity);
+	while (quantity > 0)
+	{
+		//["0!"]["0!"]["!"]
+		vector[count] = symbol;
+		count++;
+		quantity--;
+		printf("%c", vector);//[!][!][!]
+	}
+	return (0);
+}

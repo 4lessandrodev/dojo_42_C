a7ce3a431acd103fabb2b81f7d11eebb9d72f5e7
lwerneck <lwerneck@student.42sp.org.br>
2021-05-31 00:27:25 +0000

    segura essa bucha

---
+++ desafio_05/exercicio5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int	main(void){
+
+int hour;
+int minutes;
+int seconds;
+int result;
+
+printf("Digite o valor de horas: ");
+scanf("%i", &hour);
+printf("Digite o valor de minutos");
+scanf("%i", &minutes);
+print("Digite o valor de segundos");
+scanf("%i", &seconds);
+
+result = hour * 60 * 60;
+result += minutes * 60;
+result += seconds;
+
+printf("O valor total de segundos é: %i\n", result);
+
+}
